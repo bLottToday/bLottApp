2c58a1b16c2d1c8227c96c78a2578c4bab9c7b3c
Saigon BitMaster <bitmaster.sg@gmail.com>
2024-05-09 23:44:40 +0700

    bLott app

---
+++ app/src/dashboard/wonWallets.tsx
@@ -0,0 +1,134 @@
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import moment from "moment";
+import { wonWallets, ticketTxs } from "./genData";
+
+interface Column {
+  id: "date" | "wallet" | "ticketNumber" | "amount";
+  label: string;
+  minWidth?: number;
+  align?: "right";
+  format?: (value: number) => string;
+}
+
+const columns: readonly Column[] = [
+  { id: "date", label: "Date" },
+  { id: "wallet", label: "Wallet", minWidth: 100 },
+  {
+    id: "ticketNumber",
+    label: "Ticket number",
+
+    align: "right",
+    format: (value: number) => value.toLocaleString("en-US"),
+  },
+  {
+    id: "amount",
+    label: "Won amount ($ADA)",
+
+    align: "right",
+    format: (value: number) => value.toLocaleString("en-US"),
+  },
+];
+
+interface Data {
+  date: string;
+  wallet: string;
+  ticketNumber: number;
+  amount: number;
+}
+
+function createData(
+  date: string,
+  wallet: string,
+  ticketNumber: number,
+  amount: number
+): Data {
+  return { date, wallet, ticketNumber, amount };
+}
+
+const rows = wonWallets();
+
+const days = [];
+for (let i = 0; i < 30; i++) {
+  const day = moment().subtract(i, "day").format("MM-DD-YYYY").toString();
+  days.push({ id: day, date: day });
+}
+
+export default function StickyHeadTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 515 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={row.wallet}
+                  >
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
---
+++ app/src/layout/search.tsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import Box from "@mui/material/Box";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.black, 0.08),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.black, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "30ch",
+      "&:focus": {
+        width: "50ch",
+      },
+    },
+  },
+}));
+
+const style = {
+  position: "absolute" as "absolute",
+  top: 55,
+  right: 0,
+  width: 600,
+  height: "auto",
+  bgcolor: "background.paper",
+  alignSelf: "flex-start",
+  p: 2,
+};
+
+export default function SearchAppBar() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "flex-end",
+        flexDirection: "column",
+        p: 0,
+        m: 0,
+        bgcolor: "background.paper",
+        borderRadius: 1,
+      }}
+    >
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ "aria-label": "search" }}
+        />
+      </Search>
+    </Box>
+  );
+}
